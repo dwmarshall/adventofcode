77583fdf8c41485c8783aebd1f563dfb31e26e21
David Marshall <dmarshall@gmail.com>
2024-12-04 11:35:02 -0800

    2015 day 4

---
+++ 2015/04/20151204-01.py
@@ -0,0 +1,11 @@
+from hashlib import md5
+from itertools import count
+
+INPUT = "iwrupvqb"
+
+for i in count(1):
+    key = f"{INPUT}{i}"
+    digest = md5(key.encode()).hexdigest()
+    if digest.startswith("00000"):
+        print(i)
+        break
