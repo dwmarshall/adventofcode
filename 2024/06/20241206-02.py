e33992d4619c6e6c0c9cc1dc6836a1d2097eec36
David Marshall <dmarshall@gmail.com>
2024-12-06 15:41:54 -0800

    improve 2024 day 6 part 2
    My first approach was to brute force all the possible places
    a block could go, but discussion on Reddit made me understand
    that the only places a new block could go would be on places
    that the guard might be.
    
    So, first run the algorithm until the guard leaves the area,
    and then test whether adding a block at each step along the way
    would produce a loop. This reduces the number of possible block
    locations by over half.

--- 2024/06/20241206-02.py
+++ 2024/06/20241206-02.py
@@ -14,38 +14,56 @@ with open(sys.argv[1], "r") as file:
             col = line.find("^")
             initial_position = (row, col, 0)
 
+initial_visits = set()
+position = initial_position
+initial_visits.add(position)
+
+while True:
+    row, col, dir = position
+    drow, dcol = directions[dir]
+    nrow, ncol = row + drow, col + dcol
+    if nrow < 0 or nrow >= len(grid) or ncol < 0 or ncol >= len(grid[0]):
+        break
+    if grid[nrow][ncol]:
+        dir = (dir + 1) % len(directions)
+    else:
+        row, col = nrow, ncol
+    position = (row, col, dir)
+    initial_visits.add(position)
+
+possible_blockers = set([(r, c) for r, c, _ in initial_visits])
+possible_blockers.discard((initial_position[0], initial_position[1]))
+
 blockers = 0
 
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        if grid[i][j]:
-            continue
-        grid[i][j] = True
-        visited = set()
-        position = initial_position
+for i, j in possible_blockers:
+    if grid[i][j]:
+        continue
+    grid[i][j] = True
+    visited = set()
+    position = initial_position
+    visited.add(position)
+    loop_detected = False
+    while True:
+        row, col, dir = position
+        drow, dcol = directions[dir]
+        nrow, ncol = row + drow, col + dcol
+        if ncol < 0 or ncol >= len(grid[0]):
+            break
+        if nrow < 0 or nrow >= len(grid):
+            break
+        if grid[nrow][ncol]:
+            # print(f"obstacle at {nrow}, {ncol}")
+            dir = (dir + 1) % len(directions)
+        else:
+            row, col = nrow, ncol
+        position = (row, col, dir)
+        if position in visited:
+            loop_detected = True
+            break
         visited.add(position)
-        loop_detected = False
-        while True:
-            row, col, dir = position
-            drow, dcol = directions[dir]
-            nrow, ncol = row + drow, col + dcol
-            if ncol < 0 or ncol >= len(grid[0]):
-                break
-            if nrow < 0 or nrow >= len(grid):
-                break
-            if grid[nrow][ncol]:
-                # print(f"obstacle at {nrow}, {ncol}")
-                dir = (dir + 1) % len(directions)
-            else:
-                row, col = nrow, ncol
-            position = (row, col, dir)
-            if position in visited:
-                loop_detected = True
-                break
-            visited.add(position)
-        grid[i][j] = False
-        if loop_detected:
-            blockers += 1
-            print(f"created a loop with obstacle at [{i}][{j}]")
+    grid[i][j] = False
+    if loop_detected:
+        blockers += 1
 
 print(blockers)
