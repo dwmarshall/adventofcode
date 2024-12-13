009d1b0b07fad9acd06830a08d6178ca9787bc57
David Marshall <dmarshall@gmail.com>
2024-12-13 15:12:23 -0800

    2015 day 17

---
+++ 2015/17/20151217-01.py
@@ -0,0 +1,24 @@
+import sys
+
+eggnog = int(sys.argv[1])
+
+containers = []
+
+with open(sys.argv[2], "r") as file:
+    for line in file:
+        containers.append(int(line))
+
+all_containers = set(range(len(containers)))
+
+dp = [[] for _ in range(eggnog + 1)]
+dp[0].append(set())
+
+for i in range(eggnog):
+    for s in dp[i]:
+        for c in all_containers - s:
+            new_index = i + containers[c]
+            new_set = s | {c}
+            if new_index <= eggnog and new_set not in dp[new_index]:
+                dp[new_index].append(s | {c})
+
+print(len(dp[eggnog]))
