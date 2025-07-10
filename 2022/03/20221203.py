90668796f9f1f9c319afe4924063a5f7d48f433b
David Marshall <dmarshall@gmail.com>
2025-07-10 12:37:47 -0700

    2022 day 3: refactored and did part 2

--- 2022/03/20221203.py
+++ 2022/03/20221203.py
@@ -1,16 +1,34 @@
 import sys
 
-running_sum = 0
+
+def priority(x: str) -> int:
+    if ord(x) - ord("A") <= 26:
+        return 27 + ord(x) - ord("A")
+    else:
+        return 1 + ord(x) - ord("a")
+
 
 with open(sys.argv[1], "r") as file:
-    for line in file:
-        first_compartment = set(line[: len(line) // 2])
-        second_compartment = set(line[len(line) // 2 : -1])
-        common_item = (first_compartment & second_compartment).pop()
-        if ord(common_item) - ord("A") <= 26:
-            priority = 27 + ord(common_item) - ord("A")
-        else:
-            priority = 1 + ord(common_item) - ord("a")
-        running_sum += priority
+    lines = [line.rstrip("\n") for line in file]
 
-print(f"Part 1: Sum is {running_sum}")
+# Part 1
+part1_sum = 0
+
+for rucksack in lines:
+    midpoint = len(rucksack) // 2
+    first_compartment = set(rucksack[:midpoint])
+    second_compartment = set(rucksack[midpoint:])
+    common_item = (first_compartment & second_compartment).pop()
+    part1_sum += priority(common_item)
+
+print(f"Part 1: Sum is {part1_sum}")
+
+# Part 2
+
+part2_sum = 0
+
+for i in range(0, len(lines), 3):
+    common_item = set(lines[i]) & set(lines[i + 1]) & set(lines[i + 2])
+    part2_sum += priority(common_item.pop())
+
+print(f"Part 2: Sum is {part2_sum}")
